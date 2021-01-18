1be55188302c39ed6b277434660b6c4869ef5852
Kamille Norris <kamionorato@gmail.com>
2021-01-18 12:05:06 -0500

    Basic instagram profile layout, change font to open sans

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,47 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img
+                class="rounded-circle"
+                style="max-width: 150px"
+                src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/s320x320/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=Uh_JtkD6BI8AX_Xh6CO&tp=1&oh=cb0b890e9286abc690a5bc505711b191&oe=6031209F"
+            />
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <p class="pr-5"><span class="font-weight-bold">153</span> posts</p>
+                <p class="pr-5"><span class="font-weight-bold">23k</span> followers</p>
+                <p><span class="font-weight-bold">212</span> following</p>
             </div>
+            <div>
+                <p class="font-weight-bold mb-0">freeCodeCamp.org</p>
+                <p class="mb-0">We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</p>
+                <p><a href="www.freecodecamp.org">www.freecodecamp.org</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img
+                class="w-100"
+                src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c2.0.823.823a/s640x640/137282729_891157201697073_3007940832466214946_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=109&_nc_ohc=Q7dXNjyT3MIAX8bmz13&tp=1&oh=0421839a58e7dc28c28882c5770e9065&oe=602DCB8E"
+            />
+        </div>
+        <div class="col-4">
+            <img
+                class="w-100"
+                src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/e35/c139.0.550.550a/135485859_201650958336080_4564845623124856800_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=108&_nc_ohc=HnyPqCa3o6sAX8cI6AD&tp=1&oh=5fca2cc1934c05ef8522e93fcbac9581&oe=602F23DC"
+            />
+        </div>
+        <div class="col-4">
+            <img
+                class="w-100"
+                src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c53.0.900.900a/s640x640/134235244_250072063131384_630243361121788782_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=hx4cxQnGrnMAX-slGBZ&tp=1&oh=47f7026d8a41aa318122802ef851ba48&oe=602E6173"
+            />
         </div>
     </div>
 </div>
